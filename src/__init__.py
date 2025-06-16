03b97ffb02b74cfa81adb11e8a8e2c9ce2b9a6b2
Denis Gryzlov <gryzlov@gmail.com>
2025-06-16 13:51:03 +0300

    v0.1.2

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,6 @@
 """deckfs - control Stream Deck through filesystem."""
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 __author__ = "Denis Gryzlov"
 __email__ = "gryzlov@gmail.com"
 __description__ = "Linux daemon for Stream Deck control without GUI through filesystem"
